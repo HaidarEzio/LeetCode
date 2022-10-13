a3bd3651bcf288d2240fb5c5b0954859a9d41cb4
Haidar Ezio <haidar.ezio2014@gmail.com>
2022-10-13 20:47:37 +0100

    Subtree

---
+++ ts/17.Subtree.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  const areEqual = (node1: TreeNode, node2: TreeNode): boolean => {
+    if (!node1 || !node2) return !node1 && !node2;
+    if (node1.val !== node2.val) return false;
+    return areEqual(node1.left, node2.left) && areEqual(node1.right, node2.right);
+  };
+  const dfs = (node: TreeNode) => {
+    if (!node) return false;
+    if (areEqual(node, subRoot)) return true;
+    return dfs(node.left) || dfs(node.right);
+  };
+  return dfs(root);
+}
